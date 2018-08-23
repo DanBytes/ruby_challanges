57614e0137835ac3296278d46e82b8dcef891ba9
Danylo Denysov <danyldezwaan@hotmail.com>
2018-08-23 15:10:24 +1000

    Completed all chall challanges for this unit

--- conditional_challange.rb
+++ conditional_challange.rb
@@ -9,25 +9,45 @@ else
 end
 
 
-#Beast mode.
+#Beast mode / Beast mode++.
 
 your_balance = 0
+user_password = "123"
+banking_options = ["balance", "deposit", "quit"]
+application_runtime = true
 
-user_password = "mightyfalcon"
 puts "WELCOME TO THE BANKING APP"
 
-puts "What would you like to do? e.g (options balance)"
-user_options_inpput = gets.chomp
+#high quality password security :)
+puts "Enter assword to continue"
+user_password_input = gets.chomp
 
-if (user_options_inpput == "balance")
-    puts "Enter assword to continue"
-    
+while (user_password_input != user_password)
+    puts "Invalid input, try again"
     user_password_input = gets.chomp
+end
 
-    if(user_password_input === user_password)
+#options menu
+
+while application_runtime 
+    puts "What would you like to do? e.g (options: balance, deposit, quit)"
+
+    user_options_inpput = gets.chomp
+
+    if (user_options_inpput == banking_options[0])
     
         puts "Your balace is #{your_balance}"
+
+    elsif(user_options_inpput == banking_options[1])
+        puts "Your current balance is #{your_balance}"
+        puts "How much would you like to deposit?"
+        deposit_ammount = gets.chomp.to_f
+        your_balance = your_balance + deposit_ammount
+        puts "Your new balance is : #{your_balance}"
+
+    elsif(user_options_inpput == banking_options[2])
+        application_runtime = false
+    else
+        puts "invalid input"
     end
-else
-    puts "invalid input"
 end
