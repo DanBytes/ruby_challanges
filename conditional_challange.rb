bc06a17b986fdb3bab08de2818255f6c87812054
Danylo Denysov <danyldezwaan@hotmail.com>
2018-08-23 13:54:05 +1000

    Added basic banking functionality to the conditional challange

--- conditional_challange.rb
+++ conditional_challange.rb
@@ -7,3 +7,27 @@ elsif user_age == 16
 else
     puts "You are not welcome"
 end
+
+
+#Beast mode.
+
+your_balance = 0
+
+user_password = "mightyfalcon"
+puts "WELCOME TO THE BANKING APP"
+
+puts "What would you like to do? e.g (options balance)"
+user_options_inpput = gets.chomp
+
+if (user_options_inpput == "balance")
+    puts "Enter assword to continue"
+    
+    user_password_input = gets.chomp
+
+    if(user_password_input === user_password)
+    
+        puts "Your balace is #{your_balance}"
+    end
+else
+    puts "invalid input"
+end
