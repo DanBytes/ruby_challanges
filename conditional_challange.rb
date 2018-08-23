6e457e6a353aeca0f86de3d6dba8e3e94af4a013
Danylo Denysov <danyldezwaan@hotmail.com>
2018-08-23 11:30:25 +1000

    Initial upload

---
+++ conditional_challange.rb
@@ -0,0 +1,9 @@
+user_age = 16
+
+if user_age > 18
+    puts "welcome to the Casino"
+elsif user_age == 16
+    puts "Sweet Sixteen"
+else
+    puts "You are not welcome"
+end
---
+++ variable_challange.rb
@@ -0,0 +1,18 @@
+days_in_year = 365
+hours_in_year = days_in_year * 24
+minutes_in_year = hours_in_year * 60
+
+my_name = "Danylo Denysov"
+siblings = 1
+
+puts "Hours in year equals to #{hours_in_year}."
+puts "Minutes in a decade equals to #{(minutes_in_year * 10)}."
+puts "I am roughly #{minutes_in_year * 23 * 60} old in seconds."
+puts "My name is #{my_name}."
+puts "I have #{siblings} siblings."
+
+siblings = siblings + 1;
+puts "Now I have #{siblings} siblings"
+
+
+
